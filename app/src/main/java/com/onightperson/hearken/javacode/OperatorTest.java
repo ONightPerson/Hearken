b09ee094c2bff25b6c5085f9ec5525d6cbf9dfd5
liubaozhu <liubaozhu@baidu.com>
2017-09-20 11:23:22 +0800

    [feature-v1] change: Modify class name for assemble

--- app/src/main/java/com/onightperson/hearken/javacode/OperatorTest.java
+++ app/src/main/java/com/onightperson/hearken/javacode/OperatorTest.java
@@ -1,5 +1,7 @@
 package com.onightperson.hearken.javacode;
 
+import java.util.List;
+
 /**
  * Created by liubaozhu on 17/8/28.
  */
@@ -8,11 +10,16 @@ public class OperatorTest {
 
     public static void main(String[] args) {
 
-        int a = 0x1000004f;
-        int b = 0xffffffff;
+//        int a = 0x1000004f;
+//        int b = 0xffffffff;
+//
+//        long key = (long) a << 32;
+//
+//        System.out.println("key: " + Long.toHexString(key) + ", b: " + Long.toHexString(b));
 
-        long key = (long) a << 32;
-
-        System.out.println("key: " + Long.toHexString(key) + ", b: " + Long.toHexString(b));
+        List<String> names = null;
+        for (String name: names) {
+            System.out.println("name: " + name);
+        }
     }
 }
